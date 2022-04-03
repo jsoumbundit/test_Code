49c7b543a39fb4d096be2c0a8dc53f52e44d8a73
jsoumbundit <so_jirawat@yahoo.com>
2022-04-03 13:03:11 +0700

    test model 03/04/2022

--- main.py
+++ main.py
@@ -13,6 +13,46 @@ from tensorflow.keras.layers import Dense
 from tensorflow.keras import Model
 from tensorflow.keras import Input
 
+import json
+import os
+from flask import Flask
+from flask import request
+from flask import make_response
+
+# Flask app should start in global layout
+app = Flask(__name__)
+
+@app.route('/webhook', methods=['POST'])
+def webhook():
+
+    req = request.get_json(silent=True, force=True)
+    res = processRequest(req)
+    res = json.dumps(res, indent=4)
+    r = make_response(res)
+    r.headers['Content-Type'] = 'application/json'
+    return r
+
+def processRequest(req):
+
+    # Parsing the POST request body into a dictionary for easy access.
+    req_dict = json.loads(request.data)
+
+    # Accessing the fields on the POST request boduy of API.ai invocation of the webhook
+    intent = req_dict["queryResult"]["intent"]["displayName"]
+
+    res = makeWebhookResult(intent)
+
+    return res
+
+def makeWebhookResult(speech):
+
+    return {
+  "fulfillmentText": speech
+    }
+
+
+
+
 wv_model = Word2Vec.load('corpus.th.model')
 word_list = wv_model.wv.index_to_key
 
@@ -265,5 +305,11 @@ def predict_sequence(infenc, infdec, source, n_steps, cardinality):
 
 
 if __name__ == '__main__':
+    port = int(os.getenv('PORT', 5000))
+
+    print("Starting app on port %d" % port)
+
+    app.run(debug=False, port=port, host='0.0.0.0', threaded=True)
+
     #train()
-    test_model()
+    #test_model() **use this one
